f4f8d65d22d6bbc5a7a6231d903f493664ee9362
Karthiga R <karthigar@oo-u2-5524x-01.link.informatik.kit.edu>
2026-04-15 21:06:27 +0200

    minor change

--- rolladice.py
+++ rolladice.py
@@ -1,116 +1,60 @@
-# import random
-
-
-# def roll():
-#     minval = 1
-#     maxval = 6
-
-#     roll = random.randint(minval, maxval)
-#     return roll
-
-
-# while True:
-#     players = input("enter no of players (2-4):")
-#     if players.isdigit():
-#         players = int(players)
-#         if 2 <= players <= 4:
-#             break
-#         else:
-#             print("must be between 2 to 4 players")
-#     else:
-#         print("invalid try again")
-
-
-# maxscore = 50
-# player_score = [0 for _ in range(players)]
-
-# while max(player_score) < maxscore:
-
-#     for playerind in range(players):
-#         print("\n Player number", playerind+1, "turn has just started")
-
-#         print("\n your total score is:", player_score[playerind]+1)
-
-#         currentscore = 0
-
-#         while True:
-
-#             rollyn = input("Would you like to roll (Y)?")
-#             if rollyn.lower() != "y":
-#                 break
-
-#             value = roll()
-#             if value == 1:
-#                 print("your turn finish because u rolled 1")
-#                 currentscore = 0
-#                 break
-
-#             else:
-#                 print("you rolled a:", value)
-#                 currentscore += value
-
-#             print("your score is:", currentscore)
-
-#         player_score[playerind] += currentscore
-#         print("your total score is ", player_score[playerind])
-
-# maxscore = max(player_score)
-# winner = player_score.index(maxscore)
-
-# print("the winner is", winner + 1, "with a score:", maxscore)
-
 import random
 
 
 def roll():
-    min_value = 1
-    max_value = 6
-    roll = random.randint(min_value, max_value)
+    minval = 1
+    maxval = 6
 
+    roll = random.randint(minval, maxval)
     return roll
 
 
 while True:
-    players = input("Enter the number of players (2 - 4): ")
+    players = input("enter no of players (2-4):")
     if players.isdigit():
         players = int(players)
         if 2 <= players <= 4:
             break
         else:
-            print("Must be between 2 - 4 players.")
+            print("must be between 2 to 4 players")
     else:
-        print("Invalid, try again.")
+        print("invalid try again")
 
-max_score = 20
-player_scores = [0 for _ in range(players)]
 
-while max(player_scores) < max_score:
-    for player_idx in range(players):
-        print("\nPlayer number", player_idx + 1, "turn has just started!")
-        print("Your total score is:", player_scores[player_idx], "\n")
-        current_score = 0
+maxscore = 50
+player_score = [0 for _ in range(players)]
+
+while max(player_score) < maxscore:
+
+    for playerind in range(players):
+        print("\n Player number", playerind+1, "turn has just started")
+
+        print("\n your total score is:", player_score[playerind]+1)
+
+        currentscore = 0
 
         while True:
-            should_roll = input(
-                "Would you like to roll (y)? ")
-            if should_roll != "y":
+
+            rollyn = input("Would you like to roll (Y)?")
+            if rollyn.lower() != "y":
                 break
 
             value = roll()
             if value == 1:
-                print("You rolled a 1! Turn done!")
-                current_score = 0
+                print("your turn finish because u rolled 1")
+                currentscore = 0
                 break
+
             else:
-                current_score += value
-                print("You rolled a:", value)
+                print("you rolled a:", value)
+                currentscore += value
 
-            print("Your score is:", current_score)
+            print("your score is:", currentscore)
 
-        player_scores[player_idx] += current_score
-        print("Your total score is:", player_scores[player_idx])
+        player_score[playerind] += currentscore
+        print("your total score is ", player_score[playerind])
 
-max_score = max(player_scores)
-winning_idx = player_scores.index(max_score)
-print("Player number", winning_idx + 1,
-      "is the winner with a score of:", max_score)
+maxscore = max(player_score)
+winner = player_score.index(maxscore)
+
+print("the winner is", winner + 1, "with a score:", maxscore)
